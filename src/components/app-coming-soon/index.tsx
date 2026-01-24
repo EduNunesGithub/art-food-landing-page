ac07a5861ca554505258f4610de6f02600e17e06
Edu Nunes <edu.nunes.github@gmail.com>
2026-01-24 19:09:07 -0300

    style(component): small adjustments for app coming soon

--- src/components/app-coming-soon/index.tsx
+++ src/components/app-coming-soon/index.tsx
@@ -1,37 +1,122 @@
 "use client";
 
 import Image from "next/image";
+import { motion } from "motion/react";
 import { twMerge } from "tailwind-merge";
 import { Mockup } from "@/components/mockup";
 import { Button } from "@/components/ui/button";
 import mobilePlaceholder from "#/png/mobile-placeholder.png";
 
 export const AppComingSoon = () => (
-  <article className="flex flex-row items-end text-black relative w-full z-0">
+  <motion.article
+    className="flex flex-row items-end text-black relative w-full z-0"
+    initial="hidden"
+    variants={{
+      hidden: {},
+      visible: {
+        transition: {
+          staggerChildren: 0.25,
+        },
+      },
+    }}
+    viewport={{ once: true }}
+    whileInView="visible"
+  >
     <div className="flex flex-col gap-4 max-w-128 w-full z-0">
-      <h2 className="text-white">Em breve: App ArtFood</h2>
+      <motion.h2
+        className="text-white"
+        transition={{
+          duration: 1,
+          ease: "anticipate",
+        }}
+        variants={{
+          hidden: {
+            filter: "blur(0.25rem)",
+            opacity: 0,
+            x: "1.5rem",
+          },
+          visible: {
+            filter: "blur(0rem)",
+            opacity: 1,
+            x: "0rem",
+          },
+        }}
+      >
+        Em breve: App ArtFood
+      </motion.h2>
 
-      <p className="text-white">
+      <motion.p
+        className="text-white"
+        transition={{
+          duration: 0.75,
+          ease: "anticipate",
+        }}
+        variants={{
+          hidden: {
+            filter: "blur(0.25rem)",
+            opacity: 0,
+            x: "1.5rem",
+          },
+          visible: {
+            filter: "blur(0rem)",
+            opacity: 1,
+            x: "0rem",
+          },
+        }}
+      >
         Receitas, livros de receitas, menus personalizados e grupos — tudo
         conectado ao seu assistente.
-      </p>
+      </motion.p>
 
-      <Button
-        asLink
-        className="bg-transparent border border-primary rounded-full text-primary"
-        href="#"
+      <motion.div
+        transition={{
+          duration: 0.5,
+          ease: "anticipate",
+        }}
+        variants={{
+          hidden: {
+            filter: "blur(0.25rem)",
+            opacity: 0,
+            x: "1.5rem",
+          },
+          visible: {
+            filter: "blur(0rem)",
+            opacity: 1,
+            x: "0rem",
+          },
+        }}
       >
-        Entrar na lista de espera!
-      </Button>
+        <Button
+          asLink
+          className="bg-transparent border border-primary rounded-full text-primary"
+          href="#"
+        >
+          Entrar na lista de espera!
+        </Button>
+      </motion.div>
     </div>
 
-    <div
+    <motion.div
       aria-hidden
       className={twMerge(
-        "absolute duration-200 ease-standard flex h-fit items-center justify-center left-1/2 top-0 -translate-x-1/2 transition-all w-80 -z-10",
+        "absolute flex h-fit items-center justify-center left-1/2 top-0 -translate-x-1/2 w-80 -z-10",
         "max-[1024px]:opacity-25",
         "sm:left-[73.0215%]",
       )}
+      transition={{
+        duration: 1,
+        ease: "anticipate",
+      }}
+      variants={{
+        hidden: {
+          filter: "blur(0.5rem)",
+          y: "100%",
+        },
+        visible: {
+          filter: "blur(0rem)",
+          y: "0%",
+        },
+      }}
     >
       <Mockup aria-hidden>
         <Image
@@ -44,6 +129,6 @@ export const AppComingSoon = () => (
           width={512}
         />
       </Mockup>
-    </div>
-  </article>
+    </motion.div>
+  </motion.article>
 );
